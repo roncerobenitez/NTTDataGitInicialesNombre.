31ec05780f0b5c691ddcd871c0b9ac70a54b71f4
jroncerb <jroncerb@emeal.nttdata.com>
2022-10-23 20:28:59 +0200

    second commit-add 'Jesus os saluda'

--- src/saludo.java
+++ src/saludo.java
@@ -4,5 +4,6 @@ public class saludo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo");
+		System.out.println("Jesús os saluda");
 	}
 }
